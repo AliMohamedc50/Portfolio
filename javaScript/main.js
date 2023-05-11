5537e290880e7617c99506c071a0dd70621daa2b
Ali Mohamed <alicastilo1097@gmail.com>
2023-05-11 18:43:43 +0300

    add contact me

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,26 +2,6 @@
 
 let widthBarSkill = document.querySelectorAll(".bar span")
 
-
-// let skills = document.querySelector(".skills")
-
-// window.onscroll = function () {
-//     let widthBarSkillOffsetTop = widthBarSkill.offsetTop;
-
-//     let widthBarSkillOuterHeight = widthBarSkill.offsetHeight;
-
-//     let windowHeight = this.innerHeight ;
-
-//     let windoScrollTop = this.pageYOffset ;
-
-//     if(windoScrollTop > (widthBarSkillOffsetTop + widthBarSkillOuterHeight - windowHeight)) {
-//         widthBarSkill.forEach((bar) => {
-//             bar.style.width = bar.dataset.widthbar;
-//         })
-//     }
-// }
-
-
 widthBarSkill.forEach((bar) => {
     
     bar.style.width = bar.dataset.widthbar
