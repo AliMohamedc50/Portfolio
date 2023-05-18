c545706f788ada13865380c636dbcd842339e8c7
cass <alicastilo1097@gmail.com>
2023-05-19 00:51:30 +0300

    Edit

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,43 +1,70 @@
+// get icon nav 
+let menuIcon = document.getElementById("menu-icon")
+let navbar = document.querySelector(".navbar")
 
-
-let sections = document.querySelectorAll("section")
-let navLinks = document.querySelector("header nav a")
-
-
-window.onscroll = () => {
-
+menuIcon.addEventListener("click", closeNave )
 
 
 
-    sections.forEach((sec) => {
-        let top = window.scrollY ;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        // let .offsetHeight;
-
-        if (top >= offset && top < offset + height) {
-            // active navbar Links
-            navLinks.forEach(links => {
-            links.classList.remove('active');
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-        });
-    }
-    // let header = document.querySelector("header")
-
-    // header.classList.toggle("sticky", window.scrollY > 100)
-
-})
+function closeNave()  {
+    navbar.classList.toggle("active")
 }
 
 
+let sections = document.querySelectorAll("section")
+let navLinks = document.querySelectorAll("header .navbar a")
 
-let widthBarSkill = document.querySelectorAll(".bar span")
 
-widthBarSkill.forEach((bar) => {
-    bar.style.width = bar.dataset.widthbar
+// console.log(navLinks)
+navLinks.forEach((linke) => 
+    linke.onclick = function(e) {
+        // linke.addEventListener("click", closeNave )
+
+        navLinks.forEach((aa) => {
+            aa.classList.remove("active")
+        })
+        e.target.classList.add("active")
+        e.preventDefault();
+        document.querySelector(linke.dataset.scroller).scrollIntoView({
+            behavior: 'smooth' 
+        })
 })
+
+
+
+
+
+
+
+// window.onscroll = () => {
+
+//     sections.forEach((sec) => {
+//         let top = window.scrollY ;
+//         let offset = sec.offsetTop - 100;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+//         // let .offsetHeight;
+//         if (top >= offset && top < offset + height) {
+//             // active navbar Links
+//             navLinks.forEach((links) => {
+//             links.classList.remove('active');
+//             document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+//         });
+//     }
+//     // let header = document.querySelector("header")
+
+//     // header.classList.toggle("sticky", window.scrollY > 100)
+
+// })
+// }
+
+
+
+// let widthBarSkill = document.querySelectorAll(".bar span")
+
+// widthBarSkill.forEach((bar) => {
+//     bar.style.width = bar.dataset.widthbar
+// })
 
 // let links = document.querySelectorAll("nav a")
 // links.forEach((ele) => {
